cfbc4b1c730970a87b371f74ca0b4487500179dc
Nick Petersen <hyyperlite@gmail.com>
2016-08-09 13:06:52 -0500

    Added policy-options (policy-statement & prefix-list) processing

--- fg_config_conversions/juniper-set-filter-analyze.rb
+++ fg_config_conversions/juniper-set-filter-analyze.rb
@@ -17,19 +17,30 @@ where [options] are:
 ### Methods
 ##################################################
 def process_firewall fw
+  ## Temp Testing
+  # p "1: #{fw.at(1)}"
+  # p "2: #{fw.at(2)}"
+  # p "3: #{fw.at(3)}"
+  # p "4: #{fw.at(4)}"
+  # p "5: #{fw.at(5)}"
+  # p "6: #{fw.at(6)}"
+  # p "7: #{fw.at(7)}"
+  # p "8: #{fw.at(8)}"
+  # p "9: #{fw.at(9)}"
+  # p "10: #{fw.at(10)}"
+
   #### Make sure the record is of firewall sub-type family
   if fw.at(2) == :family
-
     ### Make sure the record is and ipv4 firewall filter
     if fw.at(3) == :inet  && fw.at(4) == :filter
-
         ### Ensure that a "term" has been specified then create the first key "filter"
         if fw.at(6) == :term
           $h_filters[fw.at(5)] = {} if $h_filters.has_key?(fw.at(5)) == false
-          $ipv4_term_count += 1
+
           ### Add the terms to the filter hashes and create the term detail hash structure
           if $h_filters[fw.at(5)].has_key?(fw.at(7)) == false
             $h_filters[fw.at(5)][fw.at(7)] = {:source => Hash.new, :action => nil, :'forwarding-class' => nil, :'loss-priority' => nil, :policer => nil}
+            $ipv4_term_count += 1
           end
 
           ### Populate term details (sources (and source type), action, forwarding-classs, loss-priority)
@@ -58,7 +69,7 @@ def process_firewall fw
                 when :'policer'
                   $h_filters[fw.at(5)][fw.at(7)][:policer] = fw.at(10)
                 else
-                  p "action-type not supported, skipping: #{fw.at(9)} --> #{fw.at(10)}"
+                  p "ipv4 filter: action-type not supported, skipping: #{fw.at(9)} --> #{fw.at(10)}"
                 end
             ### other filter object reference
             when :filter
@@ -67,53 +78,52 @@ def process_firewall fw
         end
 
     ### IPv6 FW filter processing
-    elsif fw.at(3) == "inet6" && fw.at(4) == "filter"
-        ### Ensure that a "term" has been specified then create the first key "filter"
-        if fw.at(6) == :term
-          $h_filters6[fw.at(5)] = {} if $h_filters6.has_key?(fw.at(5)) == false
+    elsif fw.at(3) == :inet6 && fw.at(4) == :filter
+      ### Ensure that a "term" has been specified then create the first key "filter"
+      if fw.at(6) == :term
+        $h_filters6[fw.at(5)] = {} if $h_filters6.has_key?(fw.at(5)) == false
+        ### Add the terms to the filter hashes and create the term detail hash structure
+        if $h_filters6[fw.at(5)].has_key?(fw.at(7)) == false
+          $h_filters6[fw.at(5)][fw.at(7)] = {:source => Hash.new, :action => nil, :'forwarding-class' => nil, :'loss-priority' => nil, :policer => nil}
           $ipv6_term_count += 1
-          ### Add the terms to the filter hashes and create the term detail hash structure
-          if $h_filters6[fw.at(5)].has_key?(fw.at(7)) == false
-            $h_filters6[fw.at(5)][fw.at(7)] = {:source => Hash.new, :action => nil, :'forwarding-class' => nil, :'loss-priority' => nil, :policer => nil}
-          end
-
-          ### Populate term details (sources (and source type), action, forwarding-classs, loss-priority)
-          case fw.at(8)
-            ### source detail
-            when :from
-              $h_filters6[fw.at(5)][fw.at(7)][:source][fw.at(10)] = fw.at(9)
-              $ipv4_term_source_address += 1 if fw.at(9) == :'source-address'
-              $ipv4_term_source_port += 1 if fw.at(9) == :'source-port'
-              $ipv4_term_destination_address += 1 if fw.at(9) == :'destination-address'
-              $ipv4_term_destination_port += 1 if fw.at(9) == :'destination-port'
-
-            ### action/forwarding-class/loss-priority
-            when :then
-              case fw.at(9)
-                when :accept
-                  $h_filters6[fw.at(5)][fw.at(7)][:action] = fw.at(9)
-                when :discard
-                  $h_filters6[fw.at(5)][fw.at(7)][:action] = fw.at(9)
-                when :reject
-                  $h_filters6[fw.at(5)][fw.at(7)][:action] = fw.at(9)
-                when :'forwarding-class'
-                  $h_filters6[fw.at(5)][fw.at(7)][:'forwarding-class'] = fw.at(10)
-                when :'loss-priority'
-                  $h_filters6[fw.at(5)][fw.at(7)][:'loss-priority'] = fw.at(10)
-                when :'policer'
-                  $h_filters6[fw.at(5)][fw.at(7)][:policer] = fw.at(10)
-                else
-                  p "action-type not supported, skipping: #{fw.at(9)} --> #{fw.at(10)}"
-              end
-            ### other filter object reference
-            when :filter
-              $h_filters6[fw.at(5)][fw.at(7)][:source][fw.at(9)] = :filter
-          end
         end
+
+        ### Populate term details (sources (and source type), action, forwarding-classs, loss-priority)
+        case fw.at(8)
+          ### source detail
+          when :from
+            $h_filters6[fw.at(5)][fw.at(7)][:source][fw.at(10)] = fw.at(9)
+            $ipv6_term_source_address += 1 if fw.at(9) == :'source-address'
+            $ipv6_term_source_port += 1 if fw.at(9) == :'source-port'
+            $ipv6_term_destination_address += 1 if fw.at(9) == :'destination-address'
+            $ipv6_term_destination_port += 1 if fw.at(9) == :'destination-port'
+
+          ### action/forwarding-class/loss-priority
+          when :then
+            case fw.at(9)
+              when :accept
+                $h_filters6[fw.at(5)][fw.at(7)][:action] = fw.at(9)
+              when :discard
+                $h_filters6[fw.at(5)][fw.at(7)][:action] = fw.at(9)
+              when :reject
+                $h_filters6[fw.at(5)][fw.at(7)][:action] = fw.at(9)
+              when :'forwarding-class'
+                $h_filters6[fw.at(5)][fw.at(7)][:'forwarding-class'] = fw.at(10)
+              when :'loss-priority'
+                $h_filters6[fw.at(5)][fw.at(7)][:'loss-priority'] = fw.at(10)
+              when :'policer'
+                $h_filters6[fw.at(5)][fw.at(7)][:policer] = fw.at(10)
+              else
+                p "ipv6 filter: action-type not supported, skipping: #{fw.at(9)} --> #{fw.at(10)}"
+            end
+          ### other filter object reference
+          when :filter
+            $h_filters6[fw.at(5)][fw.at(7)][:source][fw.at(9)] = :filter
+        end
+      end
 
     elsif fw.at(3) == "vpls" && fw.at(4) == "filter"
       p "this is a vpls filter"
-
     else
       return
     end
@@ -124,23 +134,89 @@ def process_firewall fw
   end
 end
 
+def process_policy_options po
+  if po.at(2) == :'prefix-list'
+    if $h_prefix_lists.has_key?(po.at(3)) == false
+      $h_prefix_lists[po.at(3)] = Array.new
+      $prefix_list_count += 1
+    end
+
+  $h_prefix_lists[po.at(3)] << po.at(4)
+  $prefix_list_address_count += 1
+
+  elsif po.at(2) == :'policy-statement'
+    if $h_policy_statements.has_key?(po.at(3)) == false
+      $h_policy_statements[po.at(3)] = Hash.new
+      $policy_statement_count += 1
+    end
+    if $h_policy_statements[po.at(3)].has_key?(po.at(5)) == false
+      $h_policy_statements[po.at(3)][po.at(5)] = {:source => Hash.new, :action => nil, :metric => nil, \
+      :'next-hop' => nil, :tag => nil, :origin => nil, :pref => nil, :'local-pref' => nil, :next => nil, \
+      :'lb-perpacket' => nil}
+
+      $policy_statement_term_count += 1
+    end
+
+    ### Populate policy-statement term details
+    case po.at(6)
+      ### source detail
+      when :from
+        $h_policy_statements[po.at(3)][po.at(5)][:source][po.at(8)] = po.at(7)
+
+      ### actions
+      when :then
+        case po.at(7)
+          when :accept
+            $h_policy_statements[po.at(3)][po.at(5)][:action] = po.at(7)
+          when :discard
+            $h_policy_statements[po.at(3)][po.at(5)][:action] = po.at(7)
+          when :reject
+            $h_policy_statements[po.at(3)][po.at(5)][:action] = po.at(7)
+          when :metric
+            $h_policy_statements[po.at(3)][po.at(5)][:metric] = po.at(8)
+          when :'next-hop'
+            $h_policy_statements[po.at(3)][po.at(5)][:'next-hop'] = po.at(8)
+          when :tag
+            $h_policy_statements[po.at(3)][po.at(5)][:tag] = po.at(9)
+          when :origin
+            $h_policy_statements[po.at(3)][po.at(5)][:origin] = po.at(8)
+          when :'local-preference'
+            $h_policy_statements[po.at(3)][po.at(5)][:'local-pref'] = po.at(8)
+          when :preference
+            $h_policy_statements[po.at(3)][po.at(5)][:pref] = po.at(8)
+          when :next
+            $h_policy_statements[po.at(3)][po.at(5)][:next] = po.at(8)
+          else
+            p "policy-statement: action-type not supported, skipping: #{po.at(7)} --> #{po.at(8)}"
+        end
+      else
+        p "policy-statement: config-type not supported, skipping: #{po.at(6)}"
+    end
+  else
+    p "policy-option: config-type not supported, skipping: #{po.at(2)}"
+  end
+end
+
 #########################################
-### Main Execution
+### Main
 #########################################
-### open oonfig file for reading
+### open config file for reading
 f = File.open opts[:configfile], "r"
 
 ### Limit execution cycles during testing
 #MAX_COUNT = 100000
 MAX_COUNT = 100000
-
 linecount = 0
+
 ### Create global hprimary data hashes
 $h_interfaces = Hash.new
 $h_filters = Hash.new
 $h_filters6 = Hash.new
+$h_prefix_lists = Hash.new
+$h_policy_statements = Hash.new
 
-### Create counter vars
+
+### Create counters
 $ipv4_term_count = 0
 $ipv4_term_accept = 0
 $ipv4_term_reject = 0
@@ -163,6 +239,10 @@ $ipv6_term_source_filter = 0
 $ipv6_term_protocol = 0
 $ipv6_term_source_port = 0
 $ipv6_term_destination_port = 0
+$prefix_list_count = 0
+$prefix_list_address_count = 0
+$policy_statement_count = 0
+$policy_statement_term_count = 0
 
 
 f.each_line do |line|
@@ -170,16 +250,20 @@ f.each_line do |line|
   line = line.split
   line.map! { |x| x.to_sym}
   case line.at(1)
-###  When line defines an interface take this action
+
+    ###  When line defines an interface take this action
     when :interfaces
       next
-### When line defines firewall take this action
+
+    ### When line defines firewall take this action
     when :firewall
       process_firewall line
-### When line defines policy-options take this action
+
+    ### When line defines policy-options take this action
     when :"policy-options"
-      next
-### All other definition types aare not yet supported so skip
+      process_policy_options line
+
+    ### All other definition types aare not yet supported so skip
     else
       break if linecount >= MAX_COUNT
       next
@@ -187,10 +271,14 @@ f.each_line do |line|
   break if linecount >= MAX_COUNT
 end
 
+create_fg_ipv4_policy
+
 f.close
 
 #pp $h_filters
-pp $h_filters6
+#pp $h_filters6
+#pp $h_prefix_lists
+pp $h_policy_statements
 #puts $h_filters.to_json
 #JSON.pretty_generate($h_filters).gsub(":", " =>")
 p ""
@@ -202,3 +290,13 @@ p "     -Total with source-address...................#{$ipv4_term_source_address
 p "     -Total with source-port......................#{$ipv4_term_source_port}"
 p "     -Total with destination-address..............#{$ipv4_term_destination_address}"
 p "     -Total with destination-port.................#{$ipv4_term_destination_port}"
+p "Total IPv6 Filters............................... #{$h_filters6.size}"
+p "  -Total IPv6 Terms...............................#{$ipv6_term_count}"
+p "     -Total with source-address...................#{$ipv6_term_source_address}"
+p "     -Total with source-port......................#{$ipv6_term_source_port}"
+p "     -Total with destination-address..............#{$ipv6_term_destination_address}"
+p "     -Total with destination-port.................#{$ipv6_term_destination_port}"
+p "Total Prefix Lists................................#{$prefix_list_count}"
+p "  -Total Addresses in Prefix Lists................#{$prefix_list_address_count}"
+p "Total Policy-Statement Filters....................#{$policy_statement_count}"
+p "  -Total Policy-Statement Terms...................#{$policy_statement_term_count}"
