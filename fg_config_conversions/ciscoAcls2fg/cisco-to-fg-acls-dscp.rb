e8c14590290a623dd5d59965ff7a118ddd14b978
Nick Petersen <hyyperlite@gmail.com>
2017-04-21 14:07:15 -0500

    Initial Commit

--- fg_config_conversions/ciscoAcls2fg/cisco-to-fg-acls-dscp.rb
+++ fg_config_conversions/ciscoAcls2fg/cisco-to-fg-acls-dscp.rb
@@ -14,9 +14,13 @@ where [options] are:
   EOS
 
   opt :filein, 'Configuration File', :type => :string, :default => 'config.conf', :short => '-i'
-  opt :fileout, 'Configuration File', :type => :string, :default => 'fg.out', :short => '-o'
-  opt :debug, 'Turn on detailed messaging', :short => '-d'
+  opt :fileout, 'Configuration File', :type => :string, :default => 'cisco-acl-dscp.out', :short => '-o'
+  opt :debug, 'Turn on detailed messaging', :short => '-d', :default => false
   opt :dscpmapfile, 'File for mapping dscp class to bin', :default => 'ref/dscp_map.txt'
+  opt :ipmap, 'Map path/file for ip protocol to names mapping', :default => 'ref/ip_protocol_nums.txt'
+  opt :icmpmap, 'Map path/file for icmp type/codes to name', :default => 'ref/icmp_type_codes.txt'
+  opt :tcpudpmap, 'Map path/file for icmp type/codes to name', :default => 'ref/tcp-udp_ports.txt'
+  opt :servicecat, 'Category to assign new created services for', :default => 'Converted'
 }
 # Argument Checks
 Trollop::die :filein, ",#{Dir.pwd}/#{opts[:filein]} does not exist" unless File.exist?(opts[:filein])
@@ -26,7 +30,7 @@ Trollop::die :filein, ",#{Dir.pwd}/#{opts[:filein]} does not exist" unless File.
 ### Methods #############
 #########################
 
-def process_acls_extended(conffile)
+def process_acls_extended(conffile, debug)
   h = Hash.new
   file = File.readlines(conffile)
   linecount = 0
@@ -53,15 +57,15 @@ def process_acls_extended(conffile)
 
         # Ensure subsequent lines are permit or deny, if so, process them for this aclname
         if nextline.at(0) == 'permit' || nextline.at(0) == 'deny'
-          h[aclname][i] = {:protocol => nil, :srcaddr => nil, :srcmask => nil, :srcport => nil,
-                                     :dstaddr => nil, :dstmask => nil, :dstport => nil}
+          h[aclname][i] = {:protocol => nil, :srcaddr => nil, :srcmask => nil, :srcportlow => nil, :srcporthigh =>  nil,\
+                                     :dstaddr => nil, :dstmask => nil, :dstportlow => nil, :dstporthigh => nil}
 
           # Check/set protoccol
           if %w[ip tcp udp icmp gre].include?(nextline.at(1))
             protocol = nextline.at(1)
             h[aclname][i][:protocol] = protocol
           else
-            p "process_acls_extended: didn't match a protocol #{nextline.at(1)}, config line: #{linecount + i}"
+            p "process_acls_extended: didn't match a protocol #{nextline.at(1)}, config line: #{linecount + i}" if debug
             next
           end
 
@@ -69,10 +73,13 @@ def process_acls_extended(conffile)
           dstoffset = 4
           srcaddr = nil
           srcmask = nil
-          srcport = nil
+          srcportlow = nil
+          srcporthigh = nil
           dstaddr = nil
           dstmask = nil
-          dstport = nil
+          dstportlow = nil
+          dstporthigh = nil
+
 
           # Check/set SOURCE detail
           if IPAddress.valid?(nextline.at(2))
@@ -89,8 +96,14 @@ def process_acls_extended(conffile)
 
             # Check for source port specification
             if nextline.at(4) == 'eq'
-               srcport= nextline.at(5)
+              srcportlow = nextline.at(5)
+              srcporthigh = nextline.at(5)
               dstoffset += 2
+
+            elsif nextline.at(4) == 'range'
+              srcportlow = nextline.at(5)
+              srcporthigh = nextline.at(6)
+              dstoffset += 3
             end
 
           elsif nextline.at(2) == 'any'
@@ -99,8 +112,14 @@ def process_acls_extended(conffile)
             dstoffset -= 1
 
             if nextline.at(3) == 'eq'
-              srcport = nextline.at(4)
+              srcportlow = nextline.at(4)
+              srcporthigh = nextline.at(4)
               dstoffset += 2
+
+            elsif nextline.at(4) == 'range'
+              srcportlow = nextline.at(4)
+              srcporthigh = nextline.at(5)
+              dstoffset += 3
             end
 
           elsif nextline.at(2) == 'host'
@@ -110,17 +129,23 @@ def process_acls_extended(conffile)
             if nextline.at(4) == 'eq'
               srcport = nextline.at(5)
               dstoffset += 2
+
+            elsif nextline.at(4) == 'range'
+              srcportlow = nextline.at(4)
+              srcporthigh = nextline.at(5)
+              dstoffset += 3
             end
 
           else
-            p "process_acls_extended: src field is not valid ip, any, or host: #{nextline.at(2)}"
+            p "process_acls_extended: src field is not valid ip, any, or host: #{nextline.at(2)}" if debug
             h[aclname].delete(i)
             next
           end
 
           h[aclname][i][:srcaddr] = srcaddr
           h[aclname][i][:srcmask] = srcmask
-          h[aclname][i][:srcport] = srcport
+          h[aclname][i][:srcportlow] = srcportlow
+          h[aclname][i][:srcporthigh] = srcporthigh
 
           # Process DESTINATION details
           if IPAddress.valid?(nextline.at(dstoffset))
@@ -136,7 +161,7 @@ def process_acls_extended(conffile)
             dstmask = oct1.to_s + '.' + oct2.to_s + '.' + oct3.to_s + '.' + oct4.to_s
 
             if nextline.at(dstoffset + 2) == 'eq'
-              dstport = nextline.at(dstoffset + 2)
+              dstport = nextline.at(dstoffset + 3)
             end
 
           elsif nextline.at(dstoffset) == 'any'
@@ -156,14 +181,14 @@ def process_acls_extended(conffile)
             end
 
           else
-            p "process_acls_extended: dstip fields not ip, any, or host: #{nextline.at(2)}"
+            p "process_acls_extended: dstip fields not ip, any, or host: #{nextline.at(2)}" if debug
             h[aclname].delete(i)
             next
           end
 
           h[aclname][i][:dstaddr] = dstaddr
           h[aclname][i][:dstmask] = dstmask
-          h[aclname][i][:dstport] = dstport
+          h[aclname][i][:dstporthigh] = dstport
 
         else
           continue = false
@@ -176,7 +201,7 @@ def process_acls_extended(conffile)
 
 end
 
-def process_class_map(conffile)
+def process_class_map(conffile, debug)
   h = Hash.new
   file = File.readlines(conffile)
   linecount = 0
@@ -208,7 +233,7 @@ def process_class_map(conffile)
           h[classmap]['dscp'] << nextline.at(2)
 
         elsif nextline.at(0) == 'match'
-          p "process_class_map: class-map match type not supported @line #{linecount += i}"
+          p "process_class_map: class-map match type not supported @line #{linecount += i}" if debug
 
         else
           continue = false
@@ -220,7 +245,7 @@ def process_class_map(conffile)
   return h
 end
 
-def process_policy_map(conffile)
+def process_policy_map(conffile, debug)
   h = Hash.new
   file = File.readlines(conffile)
   linecount = 0
@@ -266,7 +291,7 @@ def process_policy_map(conffile)
   return h
 end
 
-def dscp_file_map(mapfile)
+def map_file(mapfile, debug)
 
   h = Hash.new
   file = File.open(mapfile, 'r')
@@ -280,26 +305,102 @@ def dscp_file_map(mapfile)
   return h
 end
 
+def config_fg_addresses(h, debug)
 
+  tracker = Set.new
+  fg_config = "config firewall address\n"
+
+  h.each_key do |aclname|
+    h[aclname].each_key do |acl|
+      dstaddr = h[aclname][acl][:dstaddr]
+      dstmask = h[aclname][acl][:dstmask]
+      comment = "From Extend ACL: #{aclname}"
+
+      if dstaddr != 'all' && !tracker.include?(dstaddr)
+
+        fg_config += <<-EOS
+  edit #{dstaddr}
+    set type subnet
+    set subnet #{dstaddr} #{dstmask}
+    set comment "From #{comment}"
+  next
+        EOS
+      else
+        next
+      end
+      tracker << dstaddr
+    end
+  end
+  fg_config += "end\n\n"
+  return fg_config
+end
+
+def config_fg_services(h, debug, category)
+
+  fg_config = String.new
+
+  # Create a new service category in the local fg config file to assign all created services to
+  fg_config = <<-EOS
+config firewall service category
+  edit #{category}
+end
+
+  EOS
+
+
+  # Process service objects for all ipv4 filters first
+  h.each_key do |aclname|
+
+    h[aclname].each_key do |acl|
+      if acl[:protocol] = udp
+        p "UDP:::::"
+      end
+
+
+    end
+  end
+end
+
+
+#####################################
+### END Methods
+#####################################
 
 
 
 ##### Main #####
 # declare vars
+
 h_dscp_map = Hash.new
 h_acls_extended = Hash.new
 h_class_map = Hash.new
 h_policy_map = Hash.new
+fg_config = String.new
 
 # Get hashses config config objects and mappings
-h_acls_extended = process_acls_extended(opts[:filein])
-h_class_maps = process_class_map(opts[:filein])
-h_policy_maps = process_policy_map(opts[:filein])
-h_dscp_map = dscp_file_map(opts[:dscpmapfile])
+h_acls_extended = process_acls_extended(opts[:filein], opts[:debug])
+h_class_maps = process_class_map(opts[:filein], opts[:debug])
+h_policy_maps = process_policy_map(opts[:filein], opts[:debug])
+h_dscp_map = map_file(opts[:dscpmapfile], opts[:debug])
+h_ip_map = map_file(opts[:ipmap], opts[:debug])
+h_tcpudp_map = map_file(opts[:tcpudpmap], opts[:debug])
+h_icmp_map = map_file(opts[:icmpmap], opts[:debug])
+
+
+# Create FG config
+fg_config += config_fg_addresses(h_acls_extended, opts[:debug])
+fg_config += config_fg_services(h_acls_extended, opts[:debug], opts[:category])
+
+fileout = File.open(opts[:fileout], 'w')
+fileout.write(fg_config)
+fileout.close
 
 
 ### Quick stdout for objects
 #pp h_dscp_map
+#pp h_ip_map
+#pp h_tcpudp_map
+#pp h_icmp_map
 #pp h_class_maps
 #pp h_policy_maps
-pp h_acls_extended
+#pp h_acls_extended
